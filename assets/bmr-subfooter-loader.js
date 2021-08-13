8d79032e9f69cb5198ab12bc0fd1be881bbd56f0
Ray Sarno <r.sarno@onyxdesign.net>
2021-08-12 20:49:01 -0600

    Reworking to load from github instead of CloudFlare Pages

--- assets/bmr-subfooter-loader.js
+++ assets/bmr-subfooter-loader.js
@@ -1,3 +1,22 @@
 var bmrSubfooterScript = document.createElement('script');
-bmrSubfooterScript.setAttribute('src','http://example.com/site.js');
+bmrSubfooterScript.setAttribute('src','https://bmr-subfooter.pages.dev/assets/bmr-subfooter.js');
 document.head.appendChild(bmrSubfooterScript);
+
+
+// var bmrsfXHR = new XMLHttpRequest();
+
+// // Setup our listener to process completed requests
+// bmrsfXHR.onload = function () {
+
+// 	// Process our return data
+// 	if (bmrsfXHR.status >= 200 && bmrsfXHR.status < 300) {
+// 		// Runs when the request is successful
+// 		console.log(bmrsfXHR.responseText);
+// 	} else {
+// 		// Runs when it's not
+// 		console.log(bmrsfXHR.responseText);
+// 	}
+
+// };
+// bmrsfXHR.open('GET', 'https://bmr-subfooter.pages.dev/assets/bmr-subfooter.js');
+// bmrsfXHR.send();
