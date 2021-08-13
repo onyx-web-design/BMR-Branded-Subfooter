2a7c1f7a1cbbd84a0b1325317d87349bbbeab1e1
Ray Sarno <r.sarno@onyxdesign.net>
2021-08-12 20:56:30 -0600

    Polishing

--- assets/bmr-subfooter-loader.js
+++ assets/bmr-subfooter-loader.js
@@ -1,22 +1,3 @@
 var bmrSubfooterScript = document.createElement('script');
-bmrSubfooterScript.setAttribute('src','https://bmr-subfooter.pages.dev/assets/bmr-subfooter.js');
+bmrSubfooterScript.setAttribute('src','https://raw.githubusercontent.com/onyx-web-design/BMR-Branded-Subfooter/master/assets/bmr-subfooter.js');
 document.head.appendChild(bmrSubfooterScript);
-
-
-// var bmrsfXHR = new XMLHttpRequest();
-
-// // Setup our listener to process completed requests
-// bmrsfXHR.onload = function () {
-
-// 	// Process our return data
-// 	if (bmrsfXHR.status >= 200 && bmrsfXHR.status < 300) {
-// 		// Runs when the request is successful
-// 		console.log(bmrsfXHR.responseText);
-// 	} else {
-// 		// Runs when it's not
-// 		console.log(bmrsfXHR.responseText);
-// 	}
-
-// };
-// bmrsfXHR.open('GET', 'https://bmr-subfooter.pages.dev/assets/bmr-subfooter.js');
-// bmrsfXHR.send();
