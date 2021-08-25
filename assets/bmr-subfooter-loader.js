fc8fd5295448f45b32286d0a80cd49eed7787b94
Ray Sarno <r.sarno@onyxdesign.net>
2021-08-24 17:01:52 -0700

    Tweaks and improvements + added .htacess

--- assets/bmr-subfooter-loader.js
+++ assets/bmr-subfooter-loader.js
@@ -1,3 +1,3 @@
 var bmrSubfooterScript = document.createElement('script');
-bmrSubfooterScript.setAttribute('src','https://raw.githubusercontent.com/onyx-web-design/BMR-Branded-Subfooter/master/assets/bmr-subfooter.js');
+bmrSubfooterScript.setAttribute('src','https://onyxdesign.net/bmr-branded-subfooter/assets/bmr-subfooter.js');
 document.head.appendChild(bmrSubfooterScript);
